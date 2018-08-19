b262beda403c774b50a61899737b9e30a2c2ce3b
Arsany <arsany.awad@tum.de>
2018-08-19 18:07:48 +0200

    Delete convert_class_to_nominal.sh

--- classifier/convert_class_to_nominal.sh
+++
@@ -1,8 +0,0 @@
-#java -cp /home/ga83mix2/weka-3-8-2/weka.jar weka.filters.unsupervised.attribute.NumericToNominal -i /datasets/csvdata/fold.0.tr.arff -R last -c last -o /datasets/csvdata/nominal/fold.0.tr.arff
-java -cp /home/ga83mix2/weka-3-8-2/weka.jar weka.filters.unsupervised.attribute.NumericToNominal -i /datasets/csvdata/fold.0.te.arff -R last -c last -o /datasets/csvdata/nominal/fold.0.te.arff
-java -cp /home/ga83mix2/weka-3-8-2/weka.jar weka.filters.unsupervised.attribute.NumericToNominal -i /datasets/csvdata/fold.1.tr.arff -R last -c last -o /datasets/csvdata/nominal/fold.1.tr.arff
-java -cp /home/ga83mix2/weka-3-8-2/weka.jar weka.filters.unsupervised.attribute.NumericToNominal -i /datasets/csvdata/fold.1.te.arff -R last -c last -o /datasets/csvdata/nominal/fold.1.te.arff
-java -cp /home/ga83mix2/weka-3-8-2/weka.jar weka.filters.unsupervised.attribute.NumericToNominal -i /datasets/csvdata/fold.2.tr.arff -R last -c last -o /datasets/csvdata/nominal/fold.2.tr.arff
-java -cp /home/ga83mix2/weka-3-8-2/weka.jar weka.filters.unsupervised.attribute.NumericToNominal -i /datasets/csvdata/fold.2.te.arff -R last -c last -o /datasets/csvdata/nominal/fold.2.te.arff
-java -cp /home/ga83mix2/weka-3-8-2/weka.jar weka.filters.unsupervised.attribute.NumericToNominal -i /datasets/csvdata/fold.3.tr.arff -R last -c last -o /datasets/csvdata/nominal/fold.3.tr.arff
-java -cp /home/ga83mix2/weka-3-8-2/weka.jar weka.filters.unsupervised.attribute.NumericToNominal -i /datasets/csvdata/fold.3.te.arff -R last -c last -o /datasets/csvdata/nominal/fold.3.te.arff
